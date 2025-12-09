3ec8a98b143403145b98b422727e14c84a96b971
Alessandro Genova <alessandro.genova@kitware.com>
2025-12-09 16:12:57 -0500

    .

--- scripts/setup_tauri.sh
+++ scripts/setup_tauri.sh
@@ -20,6 +20,7 @@ python -m PyInstaller --clean --noconfirm \
         --collect-all e3sm_quickview \
         --hidden-import pkgutil \
         --hidden-import PIL \
+        --hidden-import Pillow \
         --add-binary="C:\Miniconda3\envs\quickview\Library\bin\pvpython.exe:."  \
         src/e3sm_quickview/app.py
 
